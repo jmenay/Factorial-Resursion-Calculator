90183a5d21526ff3f2267feed9b7e6fad4c1d26c
jaime7175 <47618232+jmenay@users.noreply.github.com>
2022-12-08 02:14:59 +0000

    Added Comments for informative purposes. Deleted previously commented out code.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/*
+School: Tarrant County College District 
+Teacher: Tyson McMillan 
+Date: 12/07/2022 Purpose: 
+Assignment 7 Recursion in C++ 
+Skills Gained: Factorial, Validate input, C++   */
+// credit Textbook Starting out with C++ from Control Structures to Objects Tenth Edition by Tony Gaddis
+
 #include <iostream>
 using std::cout;
 using std::cin;
@@ -12,47 +20,36 @@ int main()
 { 
    cout << "***** Factorial Calculator [n!] ***** " << endl;
   int num {};
- 
- // cin >> num;
 
   bool done = false;
-  while(!done)
+  while(!done) // for input validation
   {
-    //cout << "\n(To use the calculator please enter the integer value only)" << endl;
     cout << "\nEnter the factorial number to calculate: ";
      if (cin >> num)
      {
        if (num > 0)
-         done = true;
+         done = true; //correct int obtained exit while condition
        else if (num < 0)
          cout << "\n"<< num << " is not a positive integer. Please enter a valid number.\n" <<endl;
      }
     else 
     {
-    cout << "\n"<< "Invalid entry. Try again.\n";
+      cout << "\n"<< "Invalid entry. Try again.\n";
       cout << "\n(To use the calculator please enter the positive integer value only)";
-      cin.clear();
+      cin.clear(); //clear screen
       cin.ignore();
       cout << endl;
     }
   }
-  cout <<"\n" <<num<< "! = " << factorial(num) << ". "<< endl;
+  cout <<"\n" <<num<< "! = " << factorial(num) << ". "<< endl; // print equation and answer to screen
   
      
   }
-int factorial(int n) 
+int factorial(int n) //function to calculate factorial
   {
   if (n==0) 
     return 1;
   else
     return  n*factorial(n-1);
   }
-/*int factorial(int n, int result) 
-  {
-  if (n==0) 
-    return 1;
-  else
-    result = n*factorial(n-1);
-    return result;
-  }
-*/
+
